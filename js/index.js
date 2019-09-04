f6a7d82724b14a1580c7ee53c23406030b1fb784
Emily_Richard <richard.emily.e@gmail.com>
2019-09-03 22:19:19 -0400

    finished touchups and fixes

--- js/index.js
+++ js/index.js
@@ -1,50 +1,52 @@
 // Your code goes here
 // 10 unique selectors to manipulate:
 
-/*1*/let navbar = document.querySelector('.main-navigation');
-/*2*/let navItem = document.querySelector('nav a');
-/*3*/let logoHeading = document.querySelector('logo-heading');
-/*4*/let funBus = document.querySelector('.intro img');
-/*5*/let welcome = document.querySelector('.intro h2');
-/*6*/let adventure = document.querySelector('.img-content img');
-/*7*/let fun = document.querySelector('.inverse-content .text-content');
-/*8*/let destination = document.querySelector('.content-destination img');
-/*9*/let buttons = document.querySelectorAll('.btn');
-/*10*/let footer = document.querySelector('footer');
+let navbar = document.querySelector('.main-navigation');
+let navItems = document.querySelectorAll('nav-link');
+let funBus = document.querySelector('.intro img');
+let welcome = document.querySelector('.intro h2');
+let adventure = document.querySelector('.img-content img');
+let advText = document.querySelector('.content-section .text-content');
+let fun = document.querySelector('.inverse-content .text-content');
+let destination = document.querySelector('.content-destination img');
+let footer = document.querySelector('footer');
 
 //changes topbar grey upon hover
 navbar.addEventListener('mouseover', (event) => {
     navbar.style.backgroundColor = "grey";
     navbar.style.color = "white";
+    event.stopPropagation();
 });
 
 //back to normal
 navbar.addEventListener('mouseleave', (event) => {
     navbar.style.backgroundColor = "white";
     navbar.style.color = "black";
-});
-
-document.addEventListener('load', (event) => {
-    logoHeading.style.color = "yellow";
-    
-});
-
-//changes home button yellow upon click
-navItem.addEventListener('click', (event) => {
-    navItem.style.color = "yellow";
-    preventDefault();
-    event.stopPropagation();
-});
-
-//changes home button back to black
-navItem.addEventListener('mouseleave', (event) => {
-    navItem.style.color = "black";
     event.stopPropagation();
 });
 
 //double clicking removes image
 funBus.addEventListener('dblclick', (event)=>{
     funBus.style.display = "none";
+    event.stopPropagation();
+});
+
+//changes nav items yellow upon click
+navItems.forEach((navItem) => {
+    navItem.addEventListener('click', (event) => {
+        navItem.style.color = "yellow";
+        preventDefault();
+        event.stopPropagation();
+    });
+});
+
+//changes nav items back to black                    
+navItems.forEach((navItem) => {
+    navItem.addEventListener('mouseleave', (event) => {
+        navItem.style.color = "black";
+        preventDefault();
+        event.stopPropagation();
+    });
 });
 
 //changes "welcome" color upon document scroll
@@ -52,9 +54,11 @@ document.addEventListener('scroll', (event) => {
     welcome.style.color = "red";
 });
 
-//scrolling the page removes the adventure image!
-document.addEventListener('wheel', (event) =>{
+//dragging removes the adventure image!
+adventure.addEventListener('drag', (event) =>{
     adventure.style.display = "none";
+    advText.style.width = "100%";
+
 });
 
 //copying anything in "Adventure Awaits" changes the whole font
@@ -62,8 +66,12 @@ fun.addEventListener('copy', (event) => {
     fun.style.fontFamily = "Verdana";
 });
 
-//changes width of image on dbl click
-destination.addEventListener('dblclick',(event)=> {
-    destination.width = "50%";
-    event.stopPropagation();
+//adds border to destination image on wheel scroll
+document.addEventListener('wheel',(event)=> {
+    destination.style.border = "3px dotted green";
+});
+
+//hitting any key strikes through footer dd
+document.addEventListener('keydown', (event) => {
+    footer.style.textDecoration = "line-through";
 });
