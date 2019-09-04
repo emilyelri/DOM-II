6174b0acb854185fc5fd8fe31c4211d4c65293d2
Emily_Richard <richard.emily.e@gmail.com>
2019-09-03 21:53:44 -0400

    finished event handlers, need to polish

--- js/index.js
+++ js/index.js
@@ -7,7 +7,7 @@
 /*4*/let funBus = document.querySelector('.intro img');
 /*5*/let welcome = document.querySelector('.intro h2');
 /*6*/let adventure = document.querySelector('.img-content img');
-/*7*/let fun = document.querySelector('.inverse-content img');
+/*7*/let fun = document.querySelector('.inverse-content .text-content');
 /*8*/let destination = document.querySelector('.content-destination img');
 /*9*/let buttons = document.querySelectorAll('.btn');
 /*10*/let footer = document.querySelector('footer');
@@ -24,9 +24,15 @@ navbar.addEventListener('mouseleave', (event) => {
     navbar.style.color = "black";
 });
 
+document.addEventListener('load', (event) => {
+    logoHeading.style.color = "yellow";
+    
+});
+
 //changes home button yellow upon click
 navItem.addEventListener('click', (event) => {
     navItem.style.color = "yellow";
+    preventDefault();
     event.stopPropagation();
 });
 
@@ -41,11 +47,23 @@ funBus.addEventListener('dblclick', (event)=>{
     funBus.style.display = "none";
 });
 
-//increases h2 welcome size upon document resize
-welcome.addEventListener('resize', (event) => {
-    welcome.style.fontSize = "2rem";
+//changes "welcome" color upon document scroll
+document.addEventListener('scroll', (event) => {
+    welcome.style.color = "red";
 });
 
-adventure.addEventListener('copy', (event) =>{
+//scrolling the page removes the adventure image!
+document.addEventListener('wheel', (event) =>{
     adventure.style.display = "none";
+});
+
+//copying anything in "Adventure Awaits" changes the whole font
+fun.addEventListener('copy', (event) => {
+    fun.style.fontFamily = "Verdana";
+});
+
+//changes width of image on dbl click
+destination.addEventListener('dblclick',(event)=> {
+    destination.width = "50%";
+    event.stopPropagation();
 });
